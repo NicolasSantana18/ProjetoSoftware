05a8213db9d4a833fe0ce279aa375d528711b2f4
Nicolas Santos Santana <nicolasnss1107fdn@gmail.com>
2026-02-10 10:17:13 -0300

    Primeira aula de Java

---
+++ Aula 01/AulaInaugural/src/Variaveis.java
@@ -0,0 +1,14 @@
+public class Variaveis {
+    public static void main(String[] args) {
+
+        int idade = 35;
+
+        double altura = 1.80;
+        double peso = 85.9;
+
+        char genero = 'M'; //Caractere, só aparece uma letra ''
+        String nome = "Juan";//Conjunto de Strings ""
+
+        System.out.println("Nome: " + nome + "\nAltura: " + altura);
+    }
+}
